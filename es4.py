317774d2f247c24a4da98bf8dc66415645ee5a60
AlexTargon1 <74315028+AlexTargon1@users.noreply.github.com>
2021-01-27 22:39:59 +0100

    Delete es4.py

--- es4.py
+++
@@ -1,8 +0,0 @@
-veicoli = int(input("veicoli transitati oggi = "))
-while veicoli!=0:
-    veicoli = int(input("veicoli transitati oggi = "))
-    veicoli = (veicoli+veicoli)
-else:
-    print(veicoli)
-
-    
